02939dba612c62852745cac17fba8cc7c3a053b3
LiamSx45 <40125711+LiamSx45@users.noreply.github.com>
2022-03-23 13:37:01 -0400

    sign in and sign up pages

--- public/js/all.js
+++ public/js/all.js
@@ -1260,6 +1260,58 @@ if (typeof document !== 'undefined') {
     });
 })();
 
+(function () {
+    
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyCLSGVG_LsnWpBYCzuVdCFei-I-qf3oODI"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    //Get Elements
+    const txtEmail = document.getElementById("txtEmail");
+    const txtPassword = document.getElementById("txtPassword");
+    const btnLogin = document.getElementById("btnLogin");
+    const btnSignup = document.getElementById("btnSignup");
+
+    //Add Login Event
+    btnLogin.addEventListener('click', e => {
+        const email = txtEmail.value;
+        const password = txtPassword.value;
+
+        const auth = firebase.auth();
+
+        //sign in with firebase auth
+        auth.signInWithEmailAndPassword(email, password).then(user =>{
+            alert("Login Successful :)");
+        }).catch(err => {
+            alert(err.message);
+        });
+        
+    });
+
+    //Add Signup Event
+    btnSignup.addEventListener('click', e => {
+
+        //get email and password
+        const email = txtEmail.value;
+        const password = txtPassword.value;
+
+        const auth = firebase.auth();
+
+        //sign in with firebase auth
+        const promise = auth.createUserWithEmailAndPassword(email, password).then(user => {
+            alert("Signup Successful :)")
+        }).catch(err => {
+            alert(err.message);
+        });
+
+    });
+
+
+}());
+
 
 
 
